d89f6176d677711bc4e2f90abbbeef5d39ad19f0
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-10-04 13:59:50 -0300

    update node_install script

--- install/node_install.sh
+++ install/node_install.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-# Verifica se o NVM já está instalado
-if ! command -v nvm &>/dev/null; then
-    echo "Instalando o Node Version Manager (NVM)..."
+# Verifica se o Node já está instalado
+if ! command -v node &>/dev/null; then
+    echo "Instalando o NodeJs e o NPM..."
     
-    # Baixa e instala o NVM
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-    
-    # Carrega o NVM no ambiente atual
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    
-    # Carrega o NVM no ambiente do shell ao iniciar
-    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
-    
-    # Atualiza o ambiente para usar a versão LTS mais recente do Node.js
-    nvm install --lts
+    sudo apt install -y nodejs npm
 
     echo "Node.js foi instalado com sucesso!"
 else
-    echo "Node.js e NVM já estão instalados."
+    echo "Node.js e NPM já estão instalados."
 fi
 
