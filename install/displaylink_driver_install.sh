c2ef17ce51866f21fd70100fa447945a20f29880
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-12-13 23:46:16 -0300

    ALtera permissoes de script

--- install/displaylink_driver_install.sh
+++ install/displaylink_driver_install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 cd ~/Downloads
 wget https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb
 sudo apt install ~/Downloads/synaptics-repository-keyring.deb
